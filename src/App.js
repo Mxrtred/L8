4127d9d749eb07095d86607895a0e785eebc090d
Admin <Admin@228-06>
2024-11-22 11:33:28 +0200

    update 22.11

--- src/App.js
+++ src/App.js
@@ -26,14 +26,16 @@ function App() {
       <ImageGrid images={[
         { url: 'https://www.meme-arsenal.com/memes/063b3419c3214f2287bc7dd1232ddef5.jpg' },
         { url: 'https://avatars.mds.yandex.net/i?id=6ffb99d8e46001d9518a2a0686c86bb7_l-7069324-images-thumbs&n=13' },
-        { url: 'https://i.pinimg.com/736x/9e/97/68/9e97684a69cd2c9c4580c9e82adf2a0c.jpg' }
+        { url: 'https://i.pinimg.com/736x/9e/97/68/9e97684a69cd2c9c4580c9e82adf2a0c.jpg' },
       ]} />
       <ImageGallery
         images={[
           { url: 'https://i.pinimg.com/736x/f9/36/a5/f936a5efed8b13d15208a4ff6f346d65.jpg' },
           { url: 'https://ih1.redbubble.net/image.2479090846.2689/st,small,507x507-pad,600x600,f8f8f8.jpg' },
           { url: 'https://upload.wikimedia.org/wikipedia/ru/4/43/SwoleDogeVSCheems.jpg' },
-          { url: 'https://i.pinimg.com/736x/7b/1e/b9/7b1eb944285fe8822ebe3fc0a036e1f9.jpg' },
+          { url: 'https://steamuserimages-a.akamaihd.net/ugc/2381928551685454191/755FF732B877FB47B0CCC84E39A9A575CF1B6E1B/?imw=512&amp;imh=307&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true' },
+          { url: 'https://www.meme-arsenal.com/memes/5a361999b9e82f968d4be3b4f5263fb9.jpg' },
+          { url: 'https://yt3.ggpht.com/ytc/AMLnZu9F3qw_Xf1WcoY2wWavJvx6FQSE47i0c3I7u6dopA=s900-c-k-c0x00ffffff-no-rj' },
         ]} />
     </div>
   );
